d3a45bef83010c0a81bcd98ee907a9d7298048e3
Keiji, Yoshimi <walf443@gmail.com>
2010-04-15 08:48:32 +0900

    ちょっと外でもいじりたいので途中だけどgithubにいれとく

--- lib/Plack/Middleware/Auth/OpenID.pm
+++ lib/Plack/Middleware/Auth/OpenID.pm
@@ -2,7 +2,54 @@ package Plack::Middleware::Auth::OpenID;
 
 use strict;
 use warnings;
+use parent 'Plack::Middleware';
 our $VERSION = '0.01';
+
+use Net::OpenID::Consumer;
+use LWPx::ParanoidAgent;
+use Plack::Request;
+
+sub call {
+    my ($self, $env) = @_;
+
+    my $session = $env->{'psgix.session'};
+    unless ( $session->{$self->session_key} ) {
+        $self->open_id_auth($env);
+    }
+    if ( $session->{ $self->session_key } ) {
+        # success.
+    }
+}
+
+sub open_id_auth {
+    my ($self, $env) = @_;
+
+    # TODO: don't use Plack::Request.
+    my $req = Plack::Request->new($env);
+
+    my $csr = Net::OpenID::Consumer->new(
+        ua => LWPx::ParanoidAgent->new,
+        args => $req,
+        consumer_secret => sub { $_[0] },
+    );
+
+    if ( $req->param('open_id-check') ) {
+        if ( my $setup_url = $csr->setup_url ) {
+        } elsif ( $csr->user_cancel ) {
+        } elsif ( my $identity = $csr->verified_identity ) {
+            my $url = $identity->url;
+            $env->{'psgix.session'}->{$self->session_key} = $url;
+        }
+    } else {
+        my $trust_root = $req->uri->as_string;
+        my $return_to = $req->uri->query_form(+{ $req->uri->query_form, 'open_id-check' => 1 })->as_string;
+        my $identity = $csr->claimed_identity($self->auth_domain)
+            or die $csr->err;
+
+        my $check_url = $identity->check_url(return_to => $return_to, trust_root => $trust_root);
+        return [ 302, [ Location => $check_url ], [] ];
+    }
+}
 
 1;
 __END__
@@ -18,6 +65,10 @@ Plack::Middleware::Auth::OpenID -
 =head1 DESCRIPTION
 
 Plack::Middleware::Auth::OpenID is
+
+=head1 
+
+It's just a idea. api may change in future and not work well.
 
 =head1 AUTHOR
 
